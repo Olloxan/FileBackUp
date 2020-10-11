d8df4bae3027025544cdc9fb6f199180d6c33afe
Olloxan <axe999@gmx.de>
2020-10-11 18:51:00 +0200

    first commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace GameBackup
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
