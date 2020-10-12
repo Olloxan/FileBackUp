eb520be03f50639447a7f3b733f1118d39cfb134
Oliver Lange <oliver.lange@iwu.fraunhofer.de>
2020-10-12 12:14:24 +0200

    filemover....

---
+++ FileMover.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace GameBackup
+{
+	class FileMover
+	{
+		private string _sourcePath;
+		private string _destinationPath;
+		private Timer _FileCopyTimer;
+		private int _SaveInterval;
+		private bool _copyLock;
+
+		private int _copyCounter;
+		private int _deleteCounter;
+
+		public delegate void MessageEventHandler(string message);
+		public MessageEventHandler OnMessageReceived;
+
+		public void InitFilemover(string sourcePath, string destinationPath, int saveInterval)
+		{
+			ThrowIfNotExisting(sourcePath, destinationPath);
+			_sourcePath = sourcePath;
+			_destinationPath = destinationPath;
+			_SaveInterval = saveInterval;			
+		}
+		
+		public void Start()
+		{
+			_FileCopyTimer = new Timer(CopyFiles, new object(), 500, _SaveInterval);			
+		}
+
+		private void CopyFiles(object obj)
+		{
+			_copyCounter = 0;
+			_deleteCounter = 0;
+			_copyLock = true;			
+			CopyRecursively(_sourcePath, _destinationPath);
+			_copyLock = false;
+			ClearFilesInDestiantionDirectory(_sourcePath, _destinationPath);
+			
+			OnMessageReceived(createMessage());
+		}
+
+		string createMessage()
+		{
+			string time = DateTime.Now.ToString("H:mm:ss");
+			return string.Format("{0}: {1} Files or Folders Copied\n{2} Files or Folders deleted",time, _copyCounter, _deleteCounter);
+		}
+
+		private void ClearFilesInDestiantionDirectory(string sourcepath, string destinationpath)
+		{
+			DirectoryInfo sourceDir = new DirectoryInfo(sourcepath);
+			DirectoryInfo destinationDir = new DirectoryInfo(destinationpath);
+
+			DirectoryInfo[] sourceSubdirectories = sourceDir.GetDirectories();
+			DirectoryInfo[] destinationSubdirectories = destinationDir.GetDirectories();
+
+			FileInfo[] sourceFiles = sourceDir.GetFiles();
+			FileInfo[] destinationFiles = destinationDir.GetFiles();
+
+			foreach (FileInfo dstfile in destinationFiles)
+			{
+				if (!sourceFiles.Select(file => file.Name).Contains(dstfile.Name))
+				{
+					File.Delete(dstfile.FullName);
+					_deleteCounter++;
+				}				
+			}
+
+			foreach (DirectoryInfo dstDirectory in destinationSubdirectories)
+			{
+				if (!sourceSubdirectories.Select(dir=>dir.Name).Contains(dstDirectory.Name))
+				{
+					Directory.Delete(dstDirectory.FullName, true);
+					_deleteCounter++;
+				}
+			}
+
+			for (int i = 0; i < sourceSubdirectories.Length;i++)
+			{
+				ClearFilesInDestiantionDirectory(sourceSubdirectories[i].FullName, destinationSubdirectories[i].FullName);
+			}
+
+		}
+
+		private void CopyRecursively(string sourcepath, string destinationpath)
+		{
+			
+			DirectoryInfo info = new DirectoryInfo(sourcepath);
+			DirectoryInfo[] subdirectories = info.GetDirectories();
+
+			Directory.CreateDirectory(destinationpath);
+
+			FileInfo[] files = info.GetFiles();
+			foreach(FileInfo file in files)
+			{
+				string tempFilePath = Path.Combine(destinationpath, file.Name);
+				file.CopyTo(tempFilePath, true);
+				_copyCounter++;
+			}
+
+			foreach (DirectoryInfo subDir in subdirectories)
+			{
+				string tempPath = Path.Combine(destinationpath, subDir.Name);
+				CopyRecursively(subDir.FullName, tempPath);
+			}
+		}
+
+		public void Stop()
+		{
+			while(_copyLock)
+			{
+				Thread.Sleep(1);
+			}
+			_FileCopyTimer.Dispose();
+		}
+
+		private static void ThrowIfNotExisting(string sourcePath, string destinationPath)
+		{
+			if (DoesNotExist(sourcePath))
+			{
+				throw new ArgumentException("Source path: {0} does not exist.", sourcePath);
+			}
+			if (DoesNotExist(destinationPath))
+			{
+				throw new ArgumentException("Destination path: {0} does not exist.", destinationPath);
+			}
+		}
+
+		private static bool DoesNotExist(string destinationPath)
+		{
+			return !Directory.Exists(destinationPath);
+		}
+
+	}
+}
