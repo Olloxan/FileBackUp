8d8a4bb0fae59b957369c0fc58cb015cc9cfb3b4
Oliver Lange <oliver.lange@iwu.fraunhofer.de>
2020-10-12 12:14:10 +0200

    added copy and delete logic

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Forms;
+using System.Windows.Media;
 
 namespace GameBackup
 {
@@ -9,10 +11,18 @@ namespace GameBackup
 	public partial class MainWindow : Window
     {
 		XmlStore _store;
+		FileMover _fileMover;
+
+		
+
+		bool _running = false;
+
 		public MainWindow()
         {
             InitializeComponent();
 			_store = new XmlStore();
+			_fileMover = new FileMover();
+			_fileMover.OnMessageReceived += MessageReceived;
 			InitPathTestFields();
         }
 
@@ -44,7 +54,57 @@ namespace GameBackup
 
 		private void Btn_Start_Click(object sender, RoutedEventArgs e)
 		{
+			if (!_running)
+			{
+				StartTimer();
+				StartButtons();
+				_running = true;
+			}
+			else
+			{
+				StopTimer();
+				StopButtons();
+				_running = false;
+			}
+			
+		}
 
+		private void StartTimer()
+		{
+			_fileMover.InitFilemover(txt_FilePathSource.Text, txt_FilePathDestination.Text, 60000);
+			_fileMover.Start();
+		}
+
+		private void StopTimer()
+		{
+			_fileMover.Stop();
+		}
+
+		private void StartButtons()
+		{
+			Btn_Start.Content = "Running...";
+			Btn_Start.Background = (Brush)new BrushConverter().ConvertFrom("#FFFF3F00");
+			btn_OpenFileSource.IsEnabled = false;
+			btn_OpenFileDestination.IsEnabled = false;
+		}
+
+		private void StopButtons()
+		{
+			
+			Btn_Start.Content = "Start";
+			Btn_Start.Background = (Brush)new BrushConverter().ConvertFrom("#FF11FF00");
+			btn_OpenFileSource.IsEnabled = true;
+			btn_OpenFileDestination.IsEnabled = true;
+		}
+
+		private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+		{
+			_fileMover.Stop();
+		}
+
+		private void MessageReceived(string message)
+		{			
+			txt_Info.Dispatcher.Invoke(new Action(()=> txt_Info.Text += message + "\n"));
 		}
 	}
 }
