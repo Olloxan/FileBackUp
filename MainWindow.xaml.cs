1a58056c15eb5d460c2df13dd7b13600fd14f4d4
Oliver Lange <oliver.lange@iwu.fraunhofer.de>
2020-10-12 09:47:36 +0200

    added app config

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,15 +1,50 @@
 ﻿using System.Windows;
+using System.Windows.Forms;
 
 namespace GameBackup
 {
-    /// <summary>
-    /// Interaktionslogik für MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+	/// <summary>
+	/// Interaktionslogik für MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
     {
-        public MainWindow()
+		XmlStore _store;
+		public MainWindow()
         {
             InitializeComponent();
+			_store = new XmlStore();
+			InitPathTestFields();
         }
-    }
+
+		private void InitPathTestFields()
+		{
+			txt_FilePathSource.Text = _store.SourcePath;
+			txt_FilePathDestination.Text = _store.DestinationPath;
+		}
+
+		private void Btn_OpenFileSource_Click(object sender, RoutedEventArgs e)
+		{
+			FolderBrowserDialog fbdOpenFolder = new FolderBrowserDialog();
+			if (fbdOpenFolder.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+			{
+				txt_FilePathSource.Text = fbdOpenFolder.SelectedPath;
+				_store.SourcePath = fbdOpenFolder.SelectedPath;
+			}
+		}
+
+		private void Btn_OpenFileDestination_Click(object sender, RoutedEventArgs e)
+		{
+			FolderBrowserDialog fbdOpenFolder = new FolderBrowserDialog();
+			if (fbdOpenFolder.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+			{
+				txt_FilePathDestination.Text = fbdOpenFolder.SelectedPath;
+				_store.DestinationPath = fbdOpenFolder.SelectedPath;
+			}
+		}		
+
+		private void Btn_Start_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+	}
 }
---
+++ XmlStore.cs
@@ -0,0 +1,44 @@
+﻿using System.Configuration;
+using System.Reflection;
+
+namespace GameBackup
+{
+	public class XmlStore
+	{		
+		
+		public string SourcePath
+		{
+			get
+			{				
+				return ConfigurationManager.AppSettings.Get("SourcePath");				
+			}
+			set
+			{
+				string path = value;				
+				string exePath = Assembly.GetExecutingAssembly().Location;
+				Configuration config = ConfigurationManager.OpenExeConfiguration(exePath);
+				config.AppSettings.Settings.Remove("SourcePath");
+				config.AppSettings.Settings.Add("SourcePath", path);
+				config.Save(ConfigurationSaveMode.Modified);
+			}
+		}
+
+		public string DestinationPath
+		{
+			get
+			{
+				return ConfigurationManager.AppSettings.Get("DestinationPath");				
+			}
+
+			set
+			{
+				string path = value;				
+				string exePath = Assembly.GetExecutingAssembly().Location;
+				Configuration config = ConfigurationManager.OpenExeConfiguration(exePath);
+				config.AppSettings.Settings.Remove("DestinationPath");
+				config.AppSettings.Settings.Add("DestinationPath", path);
+				config.Save(ConfigurationSaveMode.Modified);
+			}
+		}
+	}
+}
